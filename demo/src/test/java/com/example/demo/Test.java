d3c6cfb81b8fa561ce9f7447272897cf5846aa1a
zx1006 <62635896+zx1006@users.noreply.github.com>
2020-03-30 10:26:32 +0800

    test

--- demo/src/test/java/com/example/demo/Test.java
+++ demo/src/test/java/com/example/demo/Test.java
@@ -9,5 +9,6 @@ package com.example.demo;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello git");
     }
 }
