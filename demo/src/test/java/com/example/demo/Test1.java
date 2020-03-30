c54ecdb74a71c787c588da2fe9fd9ae9567d7b08
zx1006 <15256541224@163.com>
2020-03-30 10:36:30 +0800

    test2 commit

--- demo/src/test/java/com/example/demo/Test1.java
+++ demo/src/test/java/com/example/demo/Test1.java
@@ -9,8 +9,6 @@ package com.example.demo;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("hello");
-
-
         System.out.println("world");
     }
 }
