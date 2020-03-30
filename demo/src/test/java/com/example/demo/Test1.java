b69881cfed62f1db2d5540822b44e98fab495701
zx1006 <15256541224@163.com>
2020-03-30 10:34:57 +0800

    test1 commit

---
+++ demo/src/test/java/com/example/demo/Test1.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+/**
+ * @author zhangxin
+ * @version 1.0
+ * @Description Test1
+ * @Date 2020/3/30 10:32
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+
+
+        System.out.println("world");
+    }
+}
